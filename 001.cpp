d0995e6bca088863c1ae46eda326cd4c42266ab0
itxe-del <2058129249@qq.com>
2026-02-15 22:34:37 +0800

    Add files via upload

---
+++ 001.cpp
@@ -0,0 +1,17 @@
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//	cout <<"hello world"<< endl;
+//	return 0;
+//}
+#include<iostream>
+using namespace std;
+int main()
+{
+	int num;
+	cin>>num;//cin>>ÊäÈë 
+	cout<<num;//cout>>Êä³ö 
+	return 0;
+	
+} 
